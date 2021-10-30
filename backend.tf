468faedac28af4e133244ad3bbcdfcc2e5323f84
Pirla Gangadhara Rao <60540333+ganpirla@users.noreply.github.com>
2021-10-30 23:23:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "devops-rg"
+    resource_group_name   = "multistate"
     storage_account_name  = "multitstate14594"
     container_name        = "multitstate"
     key                   = "terraform.tfstate"

77a642321a2538beb022d39d1c388eff6bfd96b0
Pirla Gangadhara Rao <60540333+ganpirla@users.noreply.github.com>
2021-10-30 21:17:48 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "multitstate"
-    storage_account_name  = "multitstate14594"
-    container_name        = "multitstate"
-    key                   = "terraform.tfstate"
-  }
-}
